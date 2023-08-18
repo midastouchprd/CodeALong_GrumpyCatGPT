ca39c6e610f994b2c08114a998b98aca49bee193
Oluwatosin Awofeso <midastouchproductions@gmail.com>
2023-08-18 12:19:45 -0500

    config fixes

--- openai-test.js
+++ openai-test.js
@@ -6,15 +6,15 @@ const YOUR_CONFIGURATION_INFO = {
     organization: process.env.ORGANIZATION, // see OpenAI docs
     apiKey: process.env.API_KEY, // see OpenAI docs
   }),
-  
-  CHANGE_ME: `(as grumpy cat but ends with a positive not-so-bad spin, and 
+
+  CHAT_GPT_SUFFIX: `(as grumpy cat but ends with a positive not-so-bad spin, and
       also not able to justify being grumpy anymore, and in less then 19 words)`
 }
 
 const openai = new OpenAIApi(YOUR_CONFIGURATION_INFO.FROM_OPEN_AI);
 
 const generateFromOpenAI = async (userPrompt) => {
-  const content = `${userPrompt} ${YOUR_CONFIGURATION_INFO.CHANGE_ME}`
+  const content = `${userPrompt} ${YOUR_CONFIGURATION_INFO.CHAT_GPT_SUFFIX}`
   console.log(content);
   try {
     const completion = await openai.createChatCompletion({
@@ -22,14 +22,14 @@ const generateFromOpenAI = async (userPrompt) => {
         messages: [{role: "user", content }],
         max_tokens: 30
     });
-    
+
     console.log(completion.data)
 
     return completion.data.choices[0].message.content
-  
+
   } catch (msg) {
     console.error(msg.response.data);
-  }  
+  }
 }
 
 export default generateFromOpenAI;
